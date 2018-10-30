00d84e4ad44193f524313ead7fa670df674819a1
Robert Conroy <rconroy293@gmail.com>
2018-10-29 22:54:05 -0700

    Update spacing

--- src/importer/log_importer.py
+++ src/importer/log_importer.py
@@ -5,7 +5,7 @@ import re
 import datetime
 
 from db.model import Expansion, Card, User, Draft, DraftSeat, Pack, PackCard, Pick
-from db.connector import getSession
+from db.connector import get_session
 
 
 SEATS = 8
@@ -24,155 +24,155 @@ LOG_DIR = 'logs'
 
 
 def importMTGOLog(session, filename):
-  if draftExists(session, filename):
-    logging.info(f'Skipping import of {filename}')
-    return
-  
-  logging.info(f'Importing draft from {filename}')
+    if draftExists(session, filename):
+        logging.info(f'Skipping import of {filename}')
+        return
+    
+    logging.info(f'Importing draft from {filename}')
 
-  with open(filename) as f:
-    draftTime = maybeGetDraftTime(f)
-    logging.debug(f'Importing draft with time {draftTime}')
-    user = maybeGetUser(f)
-    logging.debug(f'User is {user}')
-    draft, seat = initializeDraft(session, draftTime, user, filename)
+    with open(filename) as f:
+        draftTime = maybeGetDraftTime(f)
+        logging.debug(f'Importing draft with time {draftTime}')
+        user = maybeGetUser(f)
+        logging.debug(f'User is {user}')
+        draft, seat = initializeDraft(session, draftTime, user, filename)
 
-    while True:
-      expansionAbbreviation = maybeGetNextPackExpansion(f)
-      if expansionAbbreviation is None:
-        logging.debug('No more packs')
-        break
-      logging.debug(f'Next pack is {expansionAbbreviation}')
-      expansion = queryExpansion(session, expansionAbbreviation)
+        while True:
+            expansionAbbreviation = maybeGetNextPackExpansion(f)
+            if expansionAbbreviation is None:
+                logging.debug('No more packs')
+                break
+            logging.debug(f'Next pack is {expansionAbbreviation}')
+            expansion = queryExpansion(session, expansionAbbreviation)
 
-      while True:
-        pickNumber, packCardNames, pick = getNextPackCards(f)
-        logging.debug(f'Pick {pickNumber} sees {packCardNames} and takes {pick}')
-        addPack(session, seat, expansion, pickNumber, packCardNames, pick)
+            while True:
+                pickNumber, packCardNames, pick = getNextPackCards(f)
+                logging.debug(f'Pick {pickNumber} sees {packCardNames} and takes {pick}')
+                addPack(session, seat, expansion, pickNumber, packCardNames, pick)
 
-        if len(packCardNames) == 1:
-          break
+                if len(packCardNames) == 1:
+                    break
 
-  session.commit()
+    session.commit()
 
 def maybeGetDraftTime(file):
-  while True:
-    nextLine = file.readline()
-    match = TIME_REGEX.match(nextLine)
-    if match:
-      try:
-        return datetime.datetime.strptime(match.group(1), TIME_FORMAT)
-      except ValueError:
-        return None
+    while True:
+        nextLine = file.readline()
+        match = TIME_REGEX.match(nextLine)
+        if match:
+            try:
+                return datetime.datetime.strptime(match.group(1), TIME_FORMAT)
+            except ValueError:
+                return None
 
 def maybeGetUser(file):
-  while not USER_LIST_REGEX.match(file.readline()):
-    pass
+    while not USER_LIST_REGEX.match(file.readline()):
+        pass
 
-  user = None
-  while True:
-    nextLine = file.readline()
-    if nextLine == '\n':
-      return user
-    elif nextLine.startswith(PICK_INDICATOR):
-      user = nextLine[len(PICK_INDICATOR):].strip()
+    user = None
+    while True:
+        nextLine = file.readline()
+        if nextLine == '\n':
+            return user
+        elif nextLine.startswith(PICK_INDICATOR):
+            user = nextLine[len(PICK_INDICATOR):].strip()
 
 def maybeGetNextPackExpansion(file):
-  while True:
-    line = file.readline()
-    if len(line) == 0:
-      return None
+    while True:
+        line = file.readline()
+        if len(line) == 0:
+            return None
 
-    match = EXPANSION_REGEX.match(line)
-    if match:
-      return match.group(1)
+        match = EXPANSION_REGEX.match(line)
+        if match:
+            return match.group(1)
 
 def getNextPackCards(file):
-  while True:
-    nextLine = file.readline()
-    match = PACK_X_PICK_Y_REGEX.match(nextLine)
-    if match:
-      packNumber = int(match.group(1))
-      pickInPack = int(match.group(2))
-      pickNumber = packNumber * CARDS_IN_PACK + pickInPack
-      break
+    while True:
+        nextLine = file.readline()
+        match = PACK_X_PICK_Y_REGEX.match(nextLine)
+        if match:
+            packNumber = int(match.group(1))
+            pickInPack = int(match.group(2))
+            pickNumber = packNumber * CARDS_IN_PACK + pickInPack
+            break
 
-  packCardNames = []
-  while True:
-    line = file.readline()
-    if line == '\n':
-      return pickNumber, packCardNames, pick
+    packCardNames = []
+    while True:
+        line = file.readline()
+        if line == '\n':
+            return pickNumber, packCardNames, pick
 
-    if line.startswith(PICK_INDICATOR):
-      pick = line[len(PICK_INDICATOR):].strip()
-      packCardNames.append(pick)
-    else:
-      packCardNames.append(line.strip())
+        if line.startswith(PICK_INDICATOR):
+            pick = line[len(PICK_INDICATOR):].strip()
+            packCardNames.append(pick)
+        else:
+            packCardNames.append(line.strip())
 
 def initializeDraft(session, draftTime, username, name):
-  draft = Draft(start_time=draftTime, name=name)
-  session.add(draft)
+    draft = Draft(start_time=draftTime, name=name)
+    session.add(draft)
 
-  user = getOrAddUser(session, username)
+    user = getOrAddUser(session, username)
 
-  seat = DraftSeat(draft=draft.id, user=user.id)
-  session.add(seat)
+    seat = DraftSeat(draft=draft.id, user=user.id)
+    session.add(seat)
 
-  return draft, seat
+    return draft, seat
 
 def getOrAddUser(session, username):
-  if username is None:
-    username = ''
-  user = session.query(User).filter_by(username=username).one_or_none()
+    if username is None:
+        username = ''
+    user = session.query(User).filter_by(username=username).one_or_none()
 
-  if user is None:
-    logging.debug(f'Adding user {username}')
-    user = User(username=username)
-    session.add(user)
-    session.flush()
+    if user is None:
+        logging.debug(f'Adding user {username}')
+        user = User(username=username)
+        session.add(user)
+        session.flush()
 
-  return user
+    return user
 
 def addPack(session, seat, expansion, pickNumber, packCardNames, pick):
-  pack = Pack(draft_seat=seat.id, pick_number=pickNumber, expansion=expansion.id)
-  session.add(pack)
+    pack = Pack(draft_seat=seat.id, pick_number=pickNumber, expansion=expansion.id)
+    session.add(pack)
 
-  picked = False
-  for cardName in packCardNames:
-    card = queryCard(session, expansion, cardName)
-    packCard = PackCard(pack=pack.id, card=card.id)
-    session.add(packCard)
+    picked = False
+    for cardName in packCardNames:
+        card = queryCard(session, expansion, cardName)
+        packCard = PackCard(pack=pack.id, card=card.id)
+        session.add(packCard)
 
-    if cardName == pick and not picked:
-      session.flush()
-      session.add(Pick(user=seat.user, pick_number=pickNumber, pack_card=packCard.id))
-      picked = True
+        if cardName == pick and not picked:
+            session.flush()
+            session.add(Pick(user=seat.user, pick_number=pickNumber, pack_card=packCard.id))
+            picked = True
 
-  assert picked, 'No cards were picked'
+    assert picked, 'No cards were picked'
 
 def queryCard(session, expansion, cardName):
-  if SPLIT_CARD_JOIN in cardName:
-    cardName = cardName[:cardName.find(SPLIT_CARD_JOIN)]
+    if SPLIT_CARD_JOIN in cardName:
+        cardName = cardName[:cardName.find(SPLIT_CARD_JOIN)]
 
-  cardName = cardName.strip()
+    cardName = cardName.strip()
 
-  result = session.query(Card).filter_by(expansion=expansion.id, name=cardName).scalar()
-  if result:
-    return result
-
-  for suffix in SUFFIXES_TO_STRIP:
-    result = session.query(Card).filter_by(expansion=expansion.id, name=f'{cardName}{suffix}').scalar()
+    result = session.query(Card).filter_by(expansion=expansion.id, name=cardName).scalar()
     if result:
-      return result
+        return result
+
+    for suffix in SUFFIXES_TO_STRIP:
+        result = session.query(Card).filter_by(expansion=expansion.id, name=f'{cardName}{suffix}').scalar()
+        if result:
+            return result
 
 def queryExpansion(session, abbreviation):
-  return session.query(Expansion).filter_by(abbreviation=abbreviation).scalar()
+    return session.query(Expansion).filter_by(abbreviation=abbreviation).scalar()
 
 def draftExists(session, name):
-  return session.query(Draft).filter_by(name=name).one_or_none() is not None
+    return session.query(Draft).filter_by(name=name).one_or_none() is not None
 
 
 if __name__ == '__main__':
-  session = getSession()
-  for filename in os.listdir(LOG_DIR):
-    importMTGOLog(session, os.path.join(LOG_DIR, filename))
+    session = get_session()
+    for filename in os.listdir(LOG_DIR):
+        importMTGOLog(session, os.path.join(LOG_DIR, filename))
--- src/importer/set_importer.py
+++ src/importer/set_importer.py
@@ -7,81 +7,81 @@ import db.expansion_repository
 import db.card_repository
 
 from db.model import Expansion, Card
-from db.connector import getSession
+from db.connector import get_session
 
 
 JSON_FOLDER = 'sets'
 UNICODE_REPLACEMENTS = {
-  u'\u2212': '-',
-  u'\u2014': '-',
-  u'\u2022': '*',
+    u'\u2212': '-',
+    u'\u2014': '-',
+    u'\u2022': '*',
 }
 
 def normalizeStringForDb(s):
-  for (k, v) in UNICODE_REPLACEMENTS.items():
-    s = s.replace(k, v)
-  return s
+    for (k, v) in UNICODE_REPLACEMENTS.items():
+        s = s.replace(k, v)
+    return s
 
 def convertBlobToExpansion(blob):
-  name = blob['name']
-  lastLandSlot = max([int(c['number']) for c in blob['cards'] if c['type'].startswith('Basic Land')])
-  return Expansion(
-    name=name,
-    max_booster_number=lastLandSlot,
-    abbreviation=blob['code'],
-  )
+    name = blob['name']
+    lastLandSlot = max([int(c['number']) for c in blob['cards'] if c['type'].startswith('Basic Land')])
+    return Expansion(
+        name=name,
+        max_booster_number=lastLandSlot,
+        abbreviation=blob['code'],
+    )
 
 def convertBlobToCard(blob, expansionId):
-  name = normalizeStringForDb(blob['name'])
-  parsedNumber = re.match('(\\d+)(\\D*)', blob['number'])
-  number = int(parsedNumber.group(1))
-  face = None if parsedNumber.group(2) == '' else parsedNumber.group(2)
+    name = normalizeStringForDb(blob['name'])
+    parsedNumber = re.match('(\\d+)(\\D*)', blob['number'])
+    number = int(parsedNumber.group(1))
+    face = None if parsedNumber.group(2) == '' else parsedNumber.group(2)
 
-  c = Card(
-    expansion=expansionId,
-    name=name,
-    number=number,
-    type=normalizeStringForDb(blob['type']),
-    rarity=blob['rarity'],
-  )
-  if 'manaCost' in blob: c.mana_cost = blob['manaCost']
-  if 'text' in blob: c.text = normalizeStringForDb(blob['text'])
-  if 'power' in blob and blob['power'] != '*': c.power = int(blob['power'])
-  if 'toughness' in blob and blob['toughness'] != '*': c.toughness = int(blob['toughness'])
-  if 'loyalty' in blob: c.loyalty = int(blob['loyalty'])
-  if face != '': c.face = face
+    c = Card(
+        expansion=expansionId,
+        name=name,
+        number=number,
+        type=normalizeStringForDb(blob['type']),
+        rarity=blob['rarity'],
+    )
+    if 'manaCost' in blob: c.mana_cost = blob['manaCost']
+    if 'text' in blob: c.text = normalizeStringForDb(blob['text'])
+    if 'power' in blob and blob['power'] != '*': c.power = int(blob['power'])
+    if 'toughness' in blob and blob['toughness'] != '*': c.toughness = int(blob['toughness'])
+    if 'loyalty' in blob: c.loyalty = int(blob['loyalty'])
+    if face != '': c.face = face
 
-  return c
+    return c
 
 def getOrPersistExpansion(session, expansion):
-  existing = db.expansion_repository.get_expansion(session, expansion.name)
-  if existing is not None:
-    return existing
+    existing = db.expansion_repository.get_expansion(session, expansion.name)
+    if existing is not None:
+        return existing
 
-  logging.info('Adding expansion {expansion.name}')
-  session.add(expansion)
-  return expansion
+    logging.info('Adding expansion {expansion.name}')
+    session.add(expansion)
+    return expansion
 
 def getOrPersistCard(session, card):
-  existing = db.card_repository.get_card(session=session, expansion_id=card.expansion, number=card.number, face=card.face)
-  if existing is not None:
-    return existing
+    existing = db.card_repository.get_card(session=session, expansion_id=card.expansion, number=card.number, face=card.face)
+    if existing is not None:
+        return existing
 
-  logging.debug('Adding card {name}'.format(name=card.name,))
-  session.add(card)
-  return card
+    logging.debug('Adding card {name}'.format(name=card.name,))
+    session.add(card)
+    return card
 
 def addFromJson(session, blob):
-  expansion = getOrPersistExpansion(session, convertBlobToExpansion(blob))
-  session.flush()
-  for cardBlob in blob['cards']:
-    getOrPersistCard(session, convertBlobToCard(cardBlob, expansion.id))
-  session.commit()
+    expansion = getOrPersistExpansion(session, convertBlobToExpansion(blob))
+    session.flush()
+    for cardBlob in blob['cards']:
+        getOrPersistCard(session, convertBlobToCard(cardBlob, expansion.id))
+    session.commit()
 
 if __name__ == '__main__':
-  session = getSession()
-  for filename in os.listdir(JSON_FOLDER):
-    if not filename.lower().endswith('.json'):
-      continue
-    with open(os.path.join(JSON_FOLDER, filename)) as f:
-      addFromJson(session, json.load(f))
+    session = get_session()
+    for filename in os.listdir(JSON_FOLDER):
+        if not filename.lower().endswith('.json'):
+            continue
+        with open(os.path.join(JSON_FOLDER, filename)) as f:
+            addFromJson(session, json.load(f))
