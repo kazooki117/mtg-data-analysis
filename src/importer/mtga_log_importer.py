f9763fbe8d6ca2d8fb6b0179d27e5922bc150d52
Robert Conroy <rconroy293@gmail.com>
2018-11-20 23:03:19 -0800

    Start importing match info

--- src/importer/mtga_log_importer.py
+++ src/importer/mtga_log_importer.py
@@ -1,21 +1,55 @@
 import os
 
 import importer.mtga_log_helper
+import importer.deck_helper
+import db.mtga_card_repository
 
 from db.connector import get_session
 
 
 LOG_FOLDER = os.path.join('logs', 'mtga')
 
+
+def maybe_get_league_info(session, blob):
+    try:
+        blob = blob[0]
+        if blob['CurrentEventState'] != 'Completed':
+            return None
+
+        league_status = blob['ModuleInstanceData']['WinLossGate']
+        return importer.deck_helper.DeckData(
+            deck_name=blob['Id'],
+            maindeck_card_names=get_card_names(session, blob['CourseDeck']['mainDeck']),
+            sideboard_card_names=get_card_names(session, blob['CourseDeck']['sideboard']),
+            match_record=(league_status['CurrentWins'], league_status['CurrentLosses'],),
+        )
+    except KeyError as e:
+        return None
+
+def get_card_names(session, board_blob):
+    names = []
+    for d in board_blob:
+        names += d['quantity'] * [get_card_name(session, d['id']), ]
+    return names
+
+def get_card_name(session, mtga_card_id):
+    return db.mtga_card_repository.get_mtga_card(session, mtga_card_id).name
+
+
 if __name__ == '__main__':
     session = get_session()
     for filename in os.listdir(LOG_FOLDER):
+        if filename != 'sample.htm':
+            continue
         with open(os.path.join(LOG_FOLDER, filename)) as f:
             print(filename)
             blobs = importer.mtga_log_helper.get_all_log_blobs(f)
 
             for (time, blob) in blobs:
-                pass
+                maybe_league_info = maybe_get_league_info(session, blob)
+                if maybe_league_info is not None:
+                    print(maybe_league_info)
+
                 # print(time)
                 # print(blob)
                 # print()
